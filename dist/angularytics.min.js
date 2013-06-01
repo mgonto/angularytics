a8c45fe56f26230bd0c110bf6a077fea76b64ed3
Martin Gontovnikas <martin@gonto.com.ar>
2013-06-01 03:56:27 -0300

    First commit with all build tools

---
+++ dist/angularytics.min.js
@@ -0,0 +1,7 @@
+/**
+ * The solution to tracking page views and events in a SPA with AngularJS
+ * @version v0.1.0 - 2013-06-01
+ * @link https://github.com/mgonto/angularytics
+ * @author Martin Gontovnikas <martin@gonto.com.ar>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
