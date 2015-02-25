7491b0b9fee5740955da6564d2eda04c4761faba
Tuomo Jokimies <tuomo.jokimies@gmail.com>
2015-02-25 21:26:58 +0200

    Rebuild dist

--- dist/angularytics.js
+++ dist/angularytics.js
@@ -1,3 +1,10 @@
+/**
+ * The solution to tracking page views and events in a SPA with AngularJS
+ * @version v0.3.0 - 2015-02-25
+ * @link https://github.com/mgonto/angularytics
+ * @author Martin Gontovnikas <martin@gonto.com.ar>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
 (function () {
   angular.module('angularytics', []).provider('Angularytics', function () {
     var eventHandlersNames = ['Google'];
@@ -22,6 +29,7 @@
       '$rootScope',
       '$location',
       function ($injector, $rootScope, $location) {
+        // Helper methods
         var eventHandlers = [];
         angular.forEach(eventHandlersNames, function (handler) {
           eventHandlers.push($injector.get('Angularytics' + handler + 'Handler'));
@@ -32,6 +40,7 @@
           });
         };
         var service = {};
+        // Just dummy function so that it's instantiated on app creation
         service.init = function () {
         };
         service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
@@ -48,6 +57,14 @@
             }
           });
         };
+        service.trackTiming = function (category, variable, value, opt_label) {
+          forEachHandlerDo(function (handler) {
+            if (category && variable && value) {
+              handler.trackTiming(category, variable, value, opt_label);
+            }
+          });
+        };
+        // Event listening
         $rootScope.$on(pageChangeEvent, function () {
           service.trackPageView($location.url());
         });
@@ -67,39 +84,48 @@
       service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
         $log.log('Event tracked', category, action, opt_label, opt_value, opt_noninteraction);
       };
+      service.trackTiming = function (category, variable, value, opt_label) {
+        $log.log('Timing tracked', category, variable, value, opt_label);
+      };
       return service;
     }
   ]);
 }());
 (function () {
-  angular.module('angularytics').factory('AngularyticsGoogleHandler', [
-    '$log',
-    function ($log) {
-      var service = {};
-      service.trackPageView = function (url) {
-        _gaq.push([
-          '_set',
-          'page',
-          url
-        ]);
-        _gaq.push([
-          '_trackPageview',
-          url
-        ]);
-      };
-      service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
-        _gaq.push([
-          '_trackEvent',
-          category,
-          action,
-          opt_label,
-          opt_value,
-          opt_noninteraction
-        ]);
-      };
-      return service;
-    }
-  ]).factory('AngularyticsGoogleUniversalHandler', function () {
+  angular.module('angularytics').factory('AngularyticsGoogleHandler', function () {
+    var service = {};
+    service.trackPageView = function (url) {
+      _gaq.push([
+        '_set',
+        'page',
+        url
+      ]);
+      _gaq.push([
+        '_trackPageview',
+        url
+      ]);
+    };
+    service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
+      _gaq.push([
+        '_trackEvent',
+        category,
+        action,
+        opt_label,
+        opt_value,
+        opt_noninteraction
+      ]);
+    };
+    service.trackTiming = function (category, variable, value, opt_label) {
+      _gaq.push([
+        '_trackTiming',
+        category,
+        variable,
+        value,
+        opt_label
+      ]);
+    };
+    return service;
+  }).factory('AngularyticsGoogleUniversalHandler', function () {
     var service = {};
     service.trackPageView = function (url) {
       ga('set', 'page', url);
@@ -107,6 +133,9 @@
     };
     service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
       ga('send', 'event', category, action, opt_label, opt_value, { 'nonInteraction': opt_noninteraction });
+    };
+    service.trackTiming = function (category, variable, value, opt_label) {
+      ga('send', 'timing', category, variable, value, opt_label);
     };
     return service;
   });
